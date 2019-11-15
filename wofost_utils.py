cdb37866553257be1f6aa8c852f99227ab6bd5d7
Jose Gomez-Dans <jgomezdans@gmail.com>
2019-11-15 21:06:02 +0000

    Not working, needs revisiting

--- wofost_utils.py
+++ wofost_utils.py
@@ -23,6 +23,10 @@ from ipywidgets import interact, interactive, fixed
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
+regions = ["Ashanti",  "Brong_Ahafo",  "Central",  
+           "Eastern",  "Greater_Accra",  "Northern",
+           "Upper_East",  "Upper_West",  "Volta",
+           "Western"]
 
 def set_up_wofost(meteo, crop, variety, soil, mgmt, wav=100, co2=400, rdmsol=100.):
     cropdata = YAMLCropDataProvider(fpath="./WOFOST_crop_parameters")
@@ -32,7 +36,8 @@ def set_up_wofost(meteo, crop, variety, soil, mgmt, wav=100, co2=400, rdmsol=100
     sitedata = WOFOST71SiteDataProvider(WAV=wav, CO2=co2)
     parameters = ParameterProvider(cropdata=cropdata, soildata=soildata, sitedata=sitedata)
     agromanagement = YAMLAgroManagementReader("ghana_maize.amgt")
-    wdp = CABOWeatherDataProvider(meteo, fpath="./")
+
+    wdp = CABOWeatherDataProvider(meteo, fpath="./data/meteo")
     return parameters, agromanagement, wdp
     
 
@@ -47,12 +52,13 @@ def run_wofost(parameters, agromanagement, wdp, potential=True):
     return df_results, wofsim
 
 
-def change_sowing_date(sowing_date, harvest_date, n_days, meteo, crop, variety, soil, mgmt):
+def change_sowing_date(sowing_date, harvest_date, meteo, crop, variety, soil, mgmt):
     parameters, agromanagement, wdp = set_up_wofost(meteo, crop, variety, soil, mgmt,
                                                     wav=0, co2=400, rdmsol=100.)
     fig, axs = plt.subplots(nrows=2, ncols=2, sharex=True, squeeze=True,
                            figsize=(14,14))
     axs = axs.flatten()
+    n_days = (harvest_date - sowing_date).days
     while sowing_date < harvest_date:
         agromanagement[0][dt.date(
             2011, 1, 1)]['CropCalendar']['crop_start_date'] = sowing_date
@@ -71,9 +77,11 @@ def change_sowing_date(sowing_date, harvest_date, n_days, meteo, crop, variety, 
 
 def change_sowing_slider():
     interact(change_sowing_date,
-            sowing_date=widgets.DatePicker(value=dt.date(2011, 7, 1)),
-            harvest_date=widgets.DatePicker(value=dt.date(2011, 10, 1)),
-            n_days=widgets.IntSlider(value=10, min=2, max=20),
+            sowing_date=widgets.DatePicker(value=dt.date(2014, 7, 1)),
+            harvest_date=widgets.DatePicker(value=dt.date(2014, 10, 1)),
+            #meteo=widgets.Dropdown(
+            #            options=regions, value='Upper_East', description='Region:',
+            #            disabled=False,),
             meteo=fixed("Ghana"),
             crop=fixed("maize"),
             variety=fixed("Maize_VanHeemst_1988"),
