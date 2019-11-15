81be39e4771cf4eabbb4a072b11db4655959b4f6
Jose Gomez-Dans <jgomezdans@gmail.com>
2019-11-15 14:53:22 +0000

    PEM model working

--- meteo_funcs.py
+++ meteo_funcs.py
@@ -3,14 +3,22 @@
 
 import datetime as dt
 from pathlib import Path
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib
-import pandas as pd
-
 from concurrent.futures import ThreadPoolExecutor
 
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+import matplotlib
+
+import pandas as pd
+
+from scipy.interpolate import UnivariateSpline
+
+import matplotlib.dates as mdates
+
 import ipywidgets as widgets
+
 from IPython.display import display
 
 from process_meteo_drivers import grab_meteo_data
@@ -28,6 +36,22 @@ regions = ["Ashanti",  "Brong_Ahafo",  "Central",
            "Upper_East",  "Upper_West",  "Volta",
            "Western"]
 
+
+def water_limitation(precip, et):
+    return np.where(precip >= et, 1, precip/et)
+
+
+def temp_constraint(temp, t_min=12, t_max=41, t_opt=28):
+    f_temp = np.zeros_like(temp)
+    f_temp[temp < t_min] = 0.
+    f_temp = np.where(np.logical_and(t_min <= temp, temp <= t_opt),
+             (temp - t_min)/(t_opt - t_min),
+             f_temp)
+    f_temp = np.where(temp >= t_opt,
+             (t_max - temp)/(t_max-t_opt),
+             f_temp)
+    f_temp[f_temp<0] = 0.
+    return f_temp
 
 def aggregate_plots():
     start_date = dt.datetime(2010, 1, 1)
@@ -176,3 +200,146 @@ def get_region_data_func(region_name, selected_years, do_plot=True):
         plot_meteo(do_files)
     else:
         return do_files
+
+def plot_stressors_func(region_name, year):
+    
+    meteo_file = sorted([f for f in Path(f'./data/meteo/{region_name}/').glob(f"{region_name}.{year}")])[0]
+    df = pd.read_csv(meteo_file, skiprows=20, sep="\t", 
+                 names=["station", "year", "doy", "irradiance", "tmin", "tmax", "vpd", "mws", "prec"])
+    df.set_index(df['doy'])
+    et0 = calc_et0(df.irradiance.values, df.tmin.values, df.tmax.values)
+    f_water = water_limitation(df.prec.values, et0)
+    f_temp = temp_constraint(df.tmax.values)
+    plt.figure(figsize=(15, 5))
+    plt.plot(df.doy, f_water, 'o', lw=0.5, mfc="none", label="Water stress")
+    plt.plot(df.doy, f_temp, 's', lw=0.5, mfc="none", label="Heat stress")
+    plt.plot(df.doy, np.convolve(f_water, np.ones(10)/10., mode="same"), '-', lw=3, label="Smoothed Water Stress")
+    plt.plot(df.doy, np.convolve(f_temp, np.ones(10)/10., mode="same"), '-', lw=3, label="Smoothed Heat Stress")
+    plt.legend(loc="best")
+
+def plot_stressors():
+    widgets.interact(plot_stressors_func,region_name=widgets.Dropdown(
+                        options=regions, value='Upper_East', description='Region:',
+                        disabled=False,), 
+                year=widgets.IntSlider(min=2010, max=2018, value=(2015)))
+
+
+
+def meteo_calculations(year):
+    df = pd.read_csv(f"data/meteo/-022611_106965/-022611_106965.{year}", skiprows=20, sep="\t", 
+                     names=["station", "year", "doy", "irradiance", "tmin", "tmax", "vpd", "mws", "prec"])
+    df.set_index(df['doy'])
+    et0 = calc_et0(df.irradiance.values, df.tmin.values, df.tmax.values)
+    f_water = water_limitation(df.prec.values, et0)
+    f_temp = temp_constraint(df.tmax.values)
+    f_water = np.convolve(f_water, np.ones(10)/10, mode="same")
+    f_temp = np.convolve(f_temp, np.ones(5)/5, mode="same")
+    return f_water, f_temp
+
+def extract_smooth_fapar(product="fapar", year=2018, smoother=100):
+    golden_ratio = 0.61803398875
+    mask57 = 0b11100000  # Select bits 5, 6 and 7
+    product = product.lower()
+    year = year-2003
+    x = np.arange(1, 366, 8)
+    xs = np.arange(1, 366)
+    y = np.loadtxt(f"data/mcd15_{product}_2003_2018_-022611_106965.txt")[:, year]
+    qa = np.loadtxt("data/mcd15_qa_2003_2018_-022611_106965.txt", dtype=np.uint8)[:, year]
+    unc = np.power(golden_ratio, np.right_shift(np.bitwise_and(qa, mask57), 5).astype(np.float32))
+    spl = UnivariateSpline(x, y, w=(1./unc)**2)
+    spl.set_smoothing_factor(smoother)
+    return spl(xs)
+    #plt.plot(x, y)
+    #plt.plot(xs, spl(xs), 'b', lw=3)
+
+
+def crop_model():
+    
+    start_date = dt.datetime(2014, 2, 1)
+    end_date = dt.datetime(2014, 12, 1)
+
+    dates = pd.date_range(start_date, end_date, freq='D')
+
+    options = [(date.strftime(' %d %b '), date) for date in dates]
+    index = (0, len(options) - 1)
+    integration_slider = widgets.SelectionRangeSlider(
+        options=options,
+        index=index,
+        description='Sowing & Harvest',
+        orientation='horizontal',
+        layout={'width': '600px'}
+    )
+    year_widget = widgets.IntSlider(min=2010, max=2018, value=2015)
+    widgets.interact(crop_model_func, year=year_widget,
+                 integration_time = integration_slider,
+                 epsilon = widgets.fixed(0.33)
+                 )
+def crop_model_func(year, epsilon, integration_time):
+    start_date0, end_date0 = integration_time
+    start_date = dt.date(year, start_date0.month, start_date0.day)
+    end_date = dt.date(year, end_date0.month, end_date0.day)
+
+    fapar = extract_smooth_fapar(year=year)
+    f_water, f_temp = meteo_calculations(year=year)
+    gpp = epsilon*fapar*f_water*f_temp
+    plt.figure(figsize=(15, 4))
+    t_axs = pd.date_range(start=dt.date(year, 1, 1),
+                          end=dt.date(year, 12, 31))
+    fig, axs = plt.subplots(nrows=2, ncols=1, figsize=(15, 6),
+                            sharex=True,squeeze=True)
+    axs = axs.flatten()
+    axs[0].plot(t_axs, gpp)
+    
+    axs[0].axvspan(*mdates.datestr2num([start_date.strftime("%Y-%m-%d"), 
+                                     end_date.strftime("%Y-%m-%d")]), color='0.9', alpha=0.5)
+    doy_start = int(start_date.strftime("%j"))
+    doy_end = int(end_date.strftime("%j")) + 1
+
+
+
+    assim = gpp[doy_start:doy_end]
+
+    axs[1].plot(pd.date_range(start=start_date, end=end_date),
+             assim.cumsum())
+    axs[1].set_xlim(dt.date(year, 1, 1),
+             dt.date(year, 12, 31))
+    axs[0].set_ylabel("GPP [funky units]")
+    axs[1].set_ylabel(r'$\int GPP dt$')
+    _ = axs[1].set_xlabel("Time [d]")
+
+
+
+def plot_lai_stress():
+    widgets.interact(plot_lai_stress_func,
+                     year=widgets.IntSlider(min=2010, max=2018),
+                     product=widgets.Dropdown(options=["LAI", "fAPAR"]))
+def plot_lai_stress_func(year, product):
+    product = product.lower()
+    fig, axs = plt.subplots(nrows=2, ncols=1, sharex=True, squeeze=True,
+                            figsize=(15, 7))
+    axs = axs.flatten()
+    lai = np.loadtxt(f"data/mcd15_{product}_2003_2018_-022611_106965.txt")
+
+    df = pd.read_csv(f"data/meteo/-022611_106965/-022611_106965.{year}", skiprows=20, sep="\t", 
+                     names=["station", "year", "doy", "irradiance", "tmin", "tmax", "vpd", "mws", "prec"])
+    df.set_index(df['doy'])
+    et0 = calc_et0(df.irradiance.values, df.tmin.values, df.tmax.values)
+    f_water = water_limitation(df.prec.values, et0)
+    f_temp = temp_constraint(df.tmax.values)
+
+    axs[0].plot(df.doy, f_water, 'o', lw=0.5, mfc="none", label="Water stress")
+    axs[0].plot(df.doy, f_temp, 's', lw=0.5, mfc="none", label="Heat stress")
+    axs[0].plot(df.doy, np.convolve(f_water, np.ones(10)/10., mode="same"), '-', lw=3, label="Smoothed Water Stress")
+    axs[0].plot(df.doy, np.convolve(f_temp, np.ones(10)/10., mode="same"), '-', lw=3, label="Smoothed Heat Stress")
+    axs[0].legend(loc="best")
+    axs[0].set_ylabel("Stress factor [-]")
+    
+    
+    
+    if product == "fapar":
+        axs[1].plot(np.arange(1, 366, 8), lai[:, year-2003]/100, '-', lw=3)
+        axs[1].set_ylabel("fAPAR [-]")
+    else:
+        axs[1].plot(np.arange(1, 366, 8), lai[:, year-2003]/10, '-', lw=3)
+        axs[1].set_ylabel("LAI [m2/m2]")
+    axs[1].set_xlabel(f"Day of year/{year} [d]")
