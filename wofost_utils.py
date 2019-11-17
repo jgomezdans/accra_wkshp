4d50b771999fb3c616dd1e3d5f9a01b0e45ef752
Jose Gomez-Dans <jgomezdans@gmail.com>
2019-11-17 12:27:28 +0000

    Advances!

--- wofost_utils.py
+++ wofost_utils.py
@@ -27,21 +27,58 @@ regions = ["Ashanti",  "Brong_Ahafo",  "Central",
            "Eastern",  "Greater_Accra",  "Northern",
            "Upper_East",  "Upper_West",  "Volta",
            "Western"]
+agromanagement_contents = """
+Version: 1.0
+AgroManagement:
+- {year:d}-01-01:
+    CropCalendar:
+        crop_name: '{crop}'
+        variety_name: '{variety}'
+        crop_start_date: {crop_start_date}
+        crop_start_type: sowing
+        crop_end_date: {crop_end_date}
+        crop_end_type: harvest
+        max_duration: 150
+    TimedEvents: null
+    StateEvents: null
+"""
 
-def set_up_wofost(meteo, crop, variety, soil, mgmt, wav=100, co2=400, rdmsol=100.):
+
+WOFOST_PARAMETERS = ['DVS', 'LAI', 'TAGP', 'TWSO', 'TWLV', 'TWST',
+                'TWRT', 'TRA', 'RD', 'SM']
+LABELS = ["Development stage [-]", "LAI [m2/m2]",
+                 "Total Biomass [kg/ha]",
+                 "Total Storage Organ Weight [kg/ha]",
+                 "Total Leaves Weight [kg/ha]",
+                 "Total Stems Weight [kg/ha]",
+                 "Total Root Weight [kg/ha]",
+                 "Transpiration rate [cm/d]",
+                 "Rooting depth [cm]",
+                 "Soil moisture [cm3/cm3]"]
+WOFOST_LABELS = dict(zip(WOFOST_PARAMETERS, LABELS))
+
+
+
+def set_up_wofost(crop_start_date, crop_end_date,
+                  meteo, crop, variety, soil,
+                  wav=100, co2=400, rdmsol=100.):
     cropdata = YAMLCropDataProvider(fpath="./WOFOST_crop_parameters")
     cropdata.set_active_crop(crop, variety)
     soildata = CABOFileReader(soil)
     soildata['RDMSOL'] = rdmsol
     sitedata = WOFOST71SiteDataProvider(WAV=wav, CO2=co2)
     parameters = ParameterProvider(cropdata=cropdata, soildata=soildata, sitedata=sitedata)
-    agromanagement = YAMLAgroManagementReader("ghana_maize.amgt")
+    with open("temporal.amgt", 'w') as fp:
+        fp.write(agromanagement_contents.format(year=crop_start_date.year,
+                        crop=crop, variety=variety, crop_start_date=crop_start_date,
+                        crop_end_date=crop_end_date))
+    agromanagement = YAMLAgroManagementReader("temporal.amgt")
 
-    wdp = CABOWeatherDataProvider(meteo, fpath="./data/meteo/Upper_West/")
+    wdp = CABOWeatherDataProvider(meteo, fpath=f"./data/meteo/{meteo}/")
     return parameters, agromanagement, wdp
     
 
-def run_wofost(parameters, agromanagement, wdp, potential=True):
+def run_wofost(parameters, agromanagement, wdp, potential=False):
     if potential:
         wofsim = Wofost71_PP(parameters, wdp, agromanagement)
     else:
@@ -52,37 +89,41 @@ def run_wofost(parameters, agromanagement, wdp, potential=True):
     return df_results, wofsim
 
 
-def change_sowing_date(sowing_date, harvest_date, meteo, crop, variety, soil, mgmt,
+def change_sowing_date(start_sowing, end_sowing, meteo, crop, variety, soil, mgmt,
                         n_days=10):
-    parameters, agromanagement, wdp = set_up_wofost(meteo, crop, variety, soil, mgmt,
-                                                    wav=0, co2=400, rdmsol=100.,)
-    fig, axs = plt.subplots(nrows=2, ncols=2, sharex=True, squeeze=True,
-                           figsize=(14,14))
+    fig, axs = plt.subplots(nrows=5, ncols=2, sharex=True, squeeze=True,
+                           figsize=(16,16))
     axs = axs.flatten()
-    while sowing_date < harvest_date:
-        agromanagement[0][dt.date(
-            2011, 1, 1)]['CropCalendar']['crop_start_date'] = sowing_date
+    sowing_date = start_sowing
+    
+    while sowing_date < end_sowing:
+        parameters, agromanagement, wdp = set_up_wofost(
+                sowing_date, sowing_date + dt.timedelta(days=150),
+                meteo, crop, variety, soil)
+        
         df_results, simulator = run_wofost(parameters, agromanagement, wdp,
                                            potential=False)
         sowing_date += dt.timedelta(days=n_days)
-        axs[0].plot_date(df_results.index, df_results.TAGP)
-        axs[1].plot_date(df_results.index, df_results.SM)
-        axs[2].plot_date(df_results.index, df_results.LAI)
-        axs[3].plot_date(df_results.index, df_results.DVS)
+        for j, p in enumerate(WOFOST_PARAMETERS):
+            axs[j].plot_date(df_results.index, df_results[p], '-')
+            axs[j].set_ylabel(WOFOST_LABELS[p], fontsize=8)
     # fig.autofmt_xdate()
+
     plt.gcf().autofmt_xdate()
     plt.gca().fmt_xdata = matplotlib.dates.DateFormatter('%Y-%m-%d')
-    plt.xlim(dt.date(2011, 3, 1))
-    
+    plt.xlim(start_sowing, None)
+    axs[3].set_xlabel("Time [d]")
 
 def change_sowing_slider():
     interact(change_sowing_date,
-            sowing_date=widgets.DatePicker(value=dt.date(2011, 7, 1)),
-            harvest_date=widgets.DatePicker(value=dt.date(2011, 10, 1)),
-            #meteo=widgets.Dropdown(
-            #            options=regions, value='Upper_East', description='Region:',
-            #            disabled=False,),
-            meteo=fixed("Upper_West"),
+            start_sowing=widgets.DatePicker(value=dt.date(2011, 7, 1),
+            description="Earliest possible sowing date"),
+            end_sowing=widgets.DatePicker(value=dt.date(2011, 8, 10),
+            description="Latest possible sowing date"),
+            meteo=widgets.Dropdown(
+                        options=regions, value='Upper_East', description='Region:',
+                        disabled=False,),
+            n_days = widgets.IntSlider(min=1, max=20, value=10),
             crop=fixed("maize"),
             variety=fixed("Maize_VanHeemst_1988"),
             soil=fixed("ec4.new"),
